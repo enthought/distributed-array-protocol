9db8126804f03f7d74e836fb06a61d202e271543
Kurt Smith <ksmith@enthought.com>
2014-02-11 11:09:56 -0600

    Major refactoring and fixes some small bugs.

--- checker.py
+++ checker.py
@@ -43,6 +43,7 @@ def validate(distbuffer):
         return (False, msg % version)
 
     # Verify the version number.
+    # import ipdb; ipdb.set_trace()
     if strict_version.version not in ((0,9,0), (1,0,0)):
         msg = '__version__ "%s" not supported by this checker.' 
         return (False, msg % version)
@@ -76,15 +77,144 @@ def validate(distbuffer):
 
     # Verify each dim_data dict.
     for idx, dim_dict in enumerate(dim_data):
-        is_valid, msg = validate_dim_dict(idx, dim_dict, buffer.shape[idx])
+        is_valid, msg = validate_dim_dict(idx, dim_dict)
         if not is_valid:
             return (is_valid, msg)
 
     return (True, '')
 
+def _validate_common_dist_keys(idx, dim_dict):
+    # Verify presence of common keys for distributed dimensions.
+    extra, missing = _verify_exact_keys(dim_dict, 'proc_grid_rank proc_grid_size'.split())
+    if missing:
+        msg = 'keys %r missing for dimension %d.' 
+        return (False, msg % (tuple(missing), idx))
 
-def validate_dim_dict(idx, dim_dict, buffer_size):
+    # Verify process grid keys.
+    proc_grid_size = dim_dict['proc_grid_size']
+    proc_grid_rank = dim_dict['proc_grid_rank']
 
+    if not isinstance(proc_grid_size, int):
+        msg = 'proc_grid_size for dimension %d has non-integer type %r.' 
+        return (False, msg % (idx, type(proc_grid_size)))
+
+    if not isinstance(proc_grid_rank, int):
+        msg = 'proc_grid_rank for dimension %d has non-integer type %r.' 
+        return (False, msg % (idx, type(proc_grid_rank)))
+
+    if proc_grid_size < 1:
+        msg = 'proc_grid_size for dimension %d has value %d and is less than one.' 
+        return (False, msg % (idx, proc_grid_size))
+
+    if proc_grid_rank >= proc_grid_size:
+        msg = 'proc_grid_rank (value %d) >= proc_grid_size (value %d) for dimension %d.' 
+        return (False, msg % (proc_grid_rank, proc_grid_size, idx))
+
+    return (True, '')
+
+def _validate_block(idx, dim_dict):
+    ''' Verifies block distribution.'''
+    if dim_dict['dist_type'] != 'b':
+        msg = '_validate_block expecting a dist_type of "b". (given %r)'
+        raise ValueError(msg % dim_dict['dist_type'])
+    is_valid, msg = _validate_common_dist_keys(idx, dim_dict)
+    if not is_valid:
+        return (is_valid, msg)
+    for k in 'start stop'.split():
+        if k not in dim_dict:
+            msg = 'key %r not present in dictionary for distributed dimension %d.' 
+            return (False, msg % (k, idx))
+        if not isinstance(dim_dict[k], int):
+            msg = '%r for dimension %d has non-integer type %r.' 
+            return (False, msg % (k, idx, type(dim_dict[k])))
+    start = dim_dict['start']
+    stop = dim_dict['stop']
+    if start < 0:
+        msg = 'start for dimension %d has negative value %d.' 
+        return (False, msg % (idx, start))
+    if stop < start:
+        msg = 'stop (%d) for dimension %d is less than start (%d).' 
+        return (False, msg % (stop, idx, start))
+    padding = dim_dict.get('padding', (0,0))
+    if not isinstance(padding, tuple):
+        msg = 'padding (%r) for dimension %d is not a tuple.' 
+        return (False, msg % (padding, idx))
+    is_valid, msg = _validate_start(idx, dim_dict)
+    if not is_valid:
+        return (is_valid, msg)
+
+    return (True, '')
+
+def _validate_cyclic(idx, dim_dict):
+    '''Verifies cyclic (and block-cyclic) distribution.'''
+    if dim_dict['dist_type'] != 'c':
+        msg = '_validate_cyclic expecting a dist_type of "c". (given %r)'
+        raise ValueError(msg % dim_dict['dist_type'])
+    is_valid, msg = _validate_common_dist_keys(idx, dim_dict)
+    if not is_valid:
+        return (is_valid, msg)
+    if 'start' not in dim_dict:
+        msg = 'start not present in dictionary for distributed dimension %d.' 
+        return (False, msg % idx)
+    start = dim_dict['start']
+    if not isinstance(start, int):
+        msg = 'start for dimension %d has non-integer type %r.' 
+        return (False, msg % (idx, type(start)))
+    block_size = dim_dict.get('block_size', 1)
+    if not isinstance(block_size, int):
+        msg = 'block_size for dimension %d has non-integer type %r.' 
+        return (False, msg % (idx, type(block_size)))
+    if block_size < 1:
+        msg = 'block_size (%d) for dimension %d is less than one.' 
+        return (False, msg % (block_size, idx))
+    proc_grid_rank = dim_dict['proc_grid_rank']
+    if proc_grid_rank * block_size != start:
+        msg = 'proc_grid_rank * block_size != start (%d * %d != %d).' 
+        return (False, msg % (proc_grid_rank, block_size, start))
+    is_valid, msg = _validate_start(idx, dim_dict)
+    if not is_valid:
+        return (is_valid, msg)
+
+    return (True, '')
+
+def _validate_start(idx, dim_dict):
+    # Verify that start == 0 when proc_grid_rank == 0.
+    dist_type = dim_dict['dist_type']
+    proc_grid_rank = dim_dict['proc_grid_rank']
+    start = dim_dict['start']
+    if dist_type in ('b', 'c') and proc_grid_rank == 0 and start != 0:
+        msg = 'start should be zero, but has value %d for dimension %d with proc_grid_rank == %d.' 
+        return (False, msg % (start, idx, proc_grid_rank))
+
+    return (True, '')
+
+def _validate_unstructured(idx, dim_dict):
+    # Verify unstructured distribution.
+    if dim_dict['dist_type'] != 'u':
+        msg = '_validate_unstructured expecting a dist_type of "u". (given %r)'
+        raise ValueError(msg % dim_dict['dist_type'])
+    is_valid, msg = _validate_common_dist_keys(idx, dim_dict)
+    if not is_valid:
+        return (is_valid, msg)
+    if 'indices' not in dim_dict:
+        msg = 'indices not present in dictionary for distributed dimension %d.' 
+        return (False, msg % idx)
+    try:
+        memoryview(dim_dict['indices'])
+    except TypeError:
+        msg = 'indices object of type %r for distributed dimension %d does not support the buffer interface.' 
+        return (False, msg % (type(dim_dict['indices']), idx))
+    one_to_one = dim_dict.get('one_to_one', False)
+    if not isinstance(one_to_one, bool):
+        msg = 'one_to_one for dimension %d has non-boolean type %r.' 
+        return (False, msg % (idx, type(one_to_one)))
+
+    return (True, '')
+
+def _validate_undistributed(idx, dim_dict):
+    return _validate_common(idx, dim_dict)
+
+def _validate_common(idx, dim_dict):
     # Verify presence of 'dist_type' and 'size' keys.
     extra, missing = _verify_exact_keys(dim_dict, 'dist_type size'.split())
     if missing:
@@ -108,102 +238,26 @@ def validate_dim_dict(idx, dim_dict, buffer_size):
         msg = 'dimension dictionary for dimension %d has an invalid dist_type of %r' 
         return (False, msg % (idx, dist_type))
 
-    # Verify presence of common keys for distributed dimensions.
-    if dist_type != 'n': 
-        extra, missing = _verify_exact_keys(dim_dict, 'proc_grid_rank proc_grid_size'.split())
-        if missing:
-            msg = 'keys %r missing for dimension %d.' 
-            return (False, msg % (tuple(missing), idx))
-
-    # Verify process grid keys.
-    proc_grid_size = dim_dict['proc_grid_size']
-    proc_grid_rank = dim_dict['proc_grid_rank']
-
-    if not isinstance(proc_grid_size, int):
-        msg = 'proc_grid_size for dimension %d has non-integer type %r.' 
-        return (False, msg % (idx, type(proc_grid_size)))
-
-    if not isinstance(proc_grid_rank, int):
-        msg = 'proc_grid_rank for dimension %d has non-integer type %r.' 
-        return (False, msg % (idx, type(proc_grid_rank)))
-
-    if proc_grid_size < 1:
-        msg = 'proc_grid_size for dimension %d has value %d and is less than one.' 
-        return (False, msg % (idx, proc_grid_size))
-
-    if proc_grid_rank >= proc_grid_size:
-        msg = 'proc_grid_rank (value %d) >= proc_grid_size (value %d) for dimension %d.' 
-        return (False, msg % (proc_grid_rank, proc_grid_size, idx))
-
     # Verify optional periodic key.
     periodic = dim_dict.get('periodic', False)
     if not isinstance(periodic, bool):
         msg = 'Optional periodic key is present for dimension %d but has non-boolean value %r.' 
         return (False, msg % (idx, periodic))
 
-    # Verify block distribution.
-    if dist_type == 'b':
-        for k in 'start stop'.split():
-            if k not in dim_dict:
-                msg = 'key %r not present in dictionary for distributed dimension %d.' 
-                return (False, msg % (k, idx))
-            if not isinstance(dim_dict[k], int):
-                msg = '%r for dimension %d has non-integer type %r.' 
-                return (False, msg % (k, idx, type(dim_dict[k])))
-        start = dim_dict['start']
-        stop = dim_dict['stop']
-        if start < 0:
-            msg = 'start for dimension %d has negative value %d.' 
-            return (False, msg % (idx, start))
-        if stop < start:
-            msg = 'stop (%d) for dimension %d is less than start (%d).' 
-            return (False, msg % (stop, idx, start))
-        padding = dim_dict.get('padding', (0,0))
-        if not isinstance(padding, tuple):
-            msg = 'padding (%r) for dimension %d is not a tuple.' 
-            return (False, msg % (padding, idx))
-        comm_padding = (padding[0] if proc_grid_rank == 0 else 0) + \
-                (padding[1] if proc_grid_rank == proc_grid_size-1 else 0)
-        boundary_padding = sum(padding) - comm_padding
-
-    # Verify cyclic (and block-cyclic) distribution.
-    if dist_type == 'c':
-        if 'start' not in dim_dict:
-            msg = 'start not present in dictionary for distributed dimension %d.' 
-            return (False, msg % idx)
-        start = dim_dict['start']
-        if not isinstance(start, int):
-            msg = 'start for dimension %d has non-integer type %r.' 
-            return (False, msg % (idx, type(start)))
-        block_size = dim_dict.get('block_size', 1)
-        if not isinstance(block_size, int):
-            msg = 'block_size for dimension %d has non-integer type %r.' 
-            return (False, msg % (idx, type(block_size)))
-        if block_size < 1:
-            msg = 'block_size (%d) for dimension %d is less than one.' 
-            return (False, msg % (block_size, idx))
-        if proc_grid_rank * block_size != start:
-            msg = 'proc_grid_rank * block_size != start (%d * %d != %d).' 
-            return (False, msg % (proc_grid_rank, block_size, start))
-
-    # Verify that start == 0 when proc_grid_rank == 0.
-    if dist_type in ('b', 'c') and proc_grid_rank == 0 and start != 0:
-        msg = 'start should be zero, but has value %d for dimension %d with proc_grid_rank == %d.' 
-        return (False, msg % (start, idx, proc_grid_rank))
-
-    # Verify unstructured distribution.
-    if dist_type == 'u':
-        if 'indices' not in dim_dict:
-            msg = 'indices not present in dictionary for distributed dimension %d.' 
-            return (False, msg % idx)
-        try:
-            indices = memoryview(dim_dict['indices'])
-        except TypeError:
-            msg = 'indices object of type %r for distributed dimension %d does not support the buffer interface.' 
-            return (False, msg % (type(dim_dict['indices']), idx))
-        one_to_one = dim_dict.get('one_to_one', False)
-        if not isinstance(one_to_one, bool):
-            msg = 'one_to_one for dimension %d has non-boolean type %r.' 
-            return (False, msg % (idx, type(one_to_one)))
-
     return (True, '')
+
+
+def validate_dim_dict(idx, dim_dict):
+
+    dist_type = dim_dict['dist_type']
+
+    if dist_type not in set('bcun'):
+        msg = "dimension dict at index %r should have dist_type of 'b', 'c', 'u', or 'n' (given %r)" 
+        return (False, msg % (idx, dist_type))
+
+    # Select the appropriate distribution type and validate.
+    return {'b': _validate_block,
+            'c': _validate_cyclic,
+            'u': _validate_unstructured,
+            'n': _validate_undistributed,
+            }[dist_type](idx, dim_dict)
