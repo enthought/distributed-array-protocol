f64b151842783bb77dbbfa12b9125306359003bf
Robert David Grant <rgrant@enthought.com>
2014-03-30 20:35:22 -0500

    Add some passing tests.

--- validator/test_validator.py
+++ validator/test_validator.py
@@ -4,7 +4,7 @@ import numpy as np
 import validator
 
 
-class TestValidator(unittest.TestCase):
+class TestValidDimData(unittest.TestCase):
 
     def test_block(self):
         dim_data = ({'dist_type': 'b',
@@ -96,3 +96,86 @@ class TestValidator(unittest.TestCase):
                 'dim_data' : (dimdata,)}
         is_valid, msg = validator.validate(distbuffer)
         self.assertTrue(is_valid, msg)
+
+
+class TestMissingKeys(unittest.TestCase):
+
+    def test_missing_buffer(self):
+        dimdata = {
+            'dist_type':'c',
+            'size':3,
+            'proc_grid_size':4,
+            'proc_grid_rank':3,
+            'start' : 3,
+            }
+        distbuffer = {'__version__': '1.0.0',
+                'dim_data' : (dimdata,)}
+        is_valid, msg = validator.validate(distbuffer)
+        self.assertTrue(not is_valid, msg)
+
+    def test_missing_version(self):
+        dimdata = {
+            'dist_type':'c',
+            'size':3,
+            'proc_grid_size':4,
+            'proc_grid_rank':3,
+            'start' : 3,
+            }
+        distbuffer = {'buffer' : b'',
+                'dim_data' : (dimdata,)}
+        is_valid, msg = validator.validate(distbuffer)
+        self.assertTrue(not is_valid, msg)
+
+    def test_missing_dim_data(self):
+        distbuffer = {'__version__': '1.0.0',
+                'buffer' : b'',}
+        is_valid, msg = validator.validate(distbuffer)
+        self.assertTrue(not is_valid, msg)
+
+
+class TestInvalidKeys(unittest.TestCase):
+
+    def test_bad_buffer(self):
+        dimdata = {
+            'dist_type':'c',
+            'size':3,
+            'proc_grid_size':4,
+            'proc_grid_rank':3,
+            'start' : 3,
+            }
+        distbuffer = {'__version__': '1.0.0',
+                'dim_data' : (dimdata,),
+                'buffer' : [1,2,3,4],}
+        is_valid, msg = validator.validate(distbuffer)
+        self.assertTrue(not is_valid, msg)
+
+    def test_bad_version(self):
+        dimdata = {
+            'dist_type':'c',
+            'size':3,
+            'proc_grid_size':4,
+            'proc_grid_rank':3,
+            'start' : 3,
+            }
+        distbuffer = {'__version__': 'v1.0.0alpha',
+                'buffer' : b'',
+                'dim_data' : (dimdata,)}
+        is_valid, msg = validator.validate(distbuffer)
+        self.assertTrue(not is_valid, msg)
+
+
+class TestInvalidDimData(unittest.TestCase):
+
+    def test_bad_block_start(self):
+        dim_data = ({'dist_type': 'b',
+            'size': 50,
+            'proc_grid_size': 2,
+            'proc_grid_rank': 0,
+            'start': -1,
+            'stop': 50},)
+        distbuffer = {'__version__': '1.0.0',
+                'buffer': np.ones(10),
+                'dim_data': dim_data}
+
+        is_valid, msg = validator.validate(distbuffer)
+        self.assertFalse(is_valid, msg)
